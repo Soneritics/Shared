a5b681edfdb589e4996c6166cc73e9cb8090e3c7
Jordi Jolink <info@soneritics.nl>
2018-11-02 21:19:32 +0100

    Remove BodyPlain property

--- Messages.Mail/Email.cs
+++ Messages.Mail/Email.cs
@@ -9,6 +9,5 @@ namespace Messages.Mail
         public string From { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
-        public string BodyPlain { get; set; }
     }
 }
