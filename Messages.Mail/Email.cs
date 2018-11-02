9f588ac1bbff28f68755e5ba24247c0daf58f68e
Jordi Jolink <info@soneritics.nl>
2018-11-02 21:04:20 +0100

    Change Description to Subject

--- Messages.Mail/Email.cs
+++ Messages.Mail/Email.cs
@@ -7,7 +7,7 @@ namespace Messages.Mail
     {
         public string To { get; set; }
         public string From { get; set; }
-        public string Description { get; set; }
+        public string Subject { get; set; }
         public string Body { get; set; }
         public string BodyPlain { get; set; }
     }
