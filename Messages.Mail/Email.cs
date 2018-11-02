98333b6a1bc1939f57ad87accbdada2064a7c95c
Jordi Jolink <info@soneritics.nl>
2018-11-02 20:54:35 +0100

    Fix build

---
+++ Messages.Mail/Email.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Messages.Base;
+
+namespace Messages.Mail
+{
+    public class Email : BaseMessage
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Description { get; set; }
+        public string Body { get; set; }
+        public string BodyPlain { get; set; }
+    }
+}
